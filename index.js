ec05fa2058dcba43d246b0915824b198ee811144
charles-mitch200 <charlesreign2@gmail.com>
2023-07-11 23:52:53 +0300

    Contact form update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tabLinks = document.querySelectorAll(".tab-links");
 const tabContent = document.querySelectorAll(".tab-content");
-const sideMenu = document.getElementById('menuNav');
+const sideMenu = document.getElementById("menuNav");
+const popMessage = document.getElementById("msg");
 
 const openTab = (tabname) => {
   tabLinks.forEach((tab) => {
@@ -16,9 +17,26 @@ const openTab = (tabname) => {
 };
 
 const openMenu = () => {
-    sideMenu.style.right = "0";
+  sideMenu.style.right = "0";
 };
 
 const closeMenu = () => {
-    sideMenu.style.right = "-200px";
-}
+  sideMenu.style.right = "-200px";
+};
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbypQ3xlMr7uWdGQmep5N8QTc9I8yiVbIKBfx3x6Ckn1TvS0bJR-dimdR-qVgHs1U_eA/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      popMessage.innerText = "Message sent successfully!";
+      setTimeout(() => {
+        popMessage.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
